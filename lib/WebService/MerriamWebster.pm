cacc787d2e8edb7d45cb40c2fe6641b74dd9a3f4
Hao Wu <hwu@intellisurvey.com>
2014-08-10 13:26:30 -0700

    add travis

--- lib/WebService/MerriamWebster.pm
+++ lib/WebService/MerriamWebster.pm
@@ -101,9 +101,12 @@ __END__
 
 =encoding utf-8
 
+=for HTML <a href="https://travis-ci.org/swuecho/WebService_MerriamWebster"><img src="https://travis-ci.org/swuecho/WebService_MerriamWebster.svg?branch=0.09"></a>
+
 =head1 NAME
 
 WebService::MerriamWebster - use Merriam-Webster dictionary API in Perl 
+
 
 =head1 SYNOPSIS
 
